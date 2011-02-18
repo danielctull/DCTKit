8768aa86027f0f4a3357d2beb88e6f5f9e26448a
Daniel Tull <dt@danieltull.co.uk>
2011-02-18 15:06:03 +0000

    Initial commit

---
+++ DCTKit/DCTKitAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  DCTKitAppDelegate.h
+//  DCTKit
+//
+//  Created by Daniel Tull on 18.02.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCTKitAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DCTKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DCTKit
+//
+//  Created by Daniel Tull on 18.02.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
