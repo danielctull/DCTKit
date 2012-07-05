115b4591e6f504e4add33f8cc9b4db2edac14f31
Daniel Tull <dt@danieltull.co.uk>
2012-07-05 14:25:12 +0100

    Initial Commit

---
+++ DCTKit/DCTKit.h
@@ -0,0 +1,13 @@
+//
+//  DCTKit.h
+//  DCTKit
+//
+//  Created by Daniel Tull on 05.07.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCTKit : NSObject
+
+@end
---
+++ DCTKit/DCTKit.m
@@ -0,0 +1,13 @@
+//
+//  DCTKit.m
+//  DCTKit
+//
+//  Created by Daniel Tull on 05.07.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "DCTKit.h"
+
+@implementation DCTKit
+
+@end
